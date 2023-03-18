4abbb66161aa36d7d25cfcbe923c5794dccba9ae
eletas <106273412+eletas@users.noreply.github.com>
2023-03-18 23:49:49 +0100

    Kostur
    Kostur projekta

---
+++ InitialProject/InitialProject/Model/Accomodation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Entities;
+
+namespace InitialProject.Model
+{
+    public class Accomodation
+    {
+        [Key]
+        public int AccId { get; set; }
+
+        public string Name { get; set; }
+
+        public AccomodationType AccomodationType { get; set; }
+
+        public int MaxGuests { get; set; }
+
+        public int MinReservationDays { get; set; }
+
+        public int DaysBeforeCanceling { get; set; }
+
+        public List<AccomodationImage> Images { get; set; }
+     
+
+        public Accomodation() { }
+
+        public Accomodation(int accId, string name, Location location, AccomodationType type, int maxGuests, int minReservationDays, int daysBeforeCanceling)
+        {
+            AccId = accId;
+            Name = name;
+            AccomodationType = type;
+            MaxGuests = maxGuests;
+            MinReservationDays = minReservationDays;
+            DaysBeforeCanceling = daysBeforeCanceling;
+
+        }
+    }
+}
---
+++ InitialProject/InitialProject/Model/GuestRating.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Model
+{
+    public class GuestRating
+    {
+        public int Id { get; set; }
+
+        public DateTime RatingExperationDate { get; set; }
+
+        public int Cleanliness { get; set; }
+
+        public int RuleCompliance { get; set; }
+
+        public GuestRating(int id, DateTime ratingExperationDate, int cleanliness, int ruleCompliance)
+        {
+            Id = id;
+            RatingExperationDate = ratingExperationDate;
+            Cleanliness = cleanliness;
+            RuleCompliance = ruleCompliance;
+        }
+    }
+}
