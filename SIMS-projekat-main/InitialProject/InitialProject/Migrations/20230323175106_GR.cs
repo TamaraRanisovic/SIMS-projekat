a0eb67e487f9861afa12727dbae8524ca7dfe50a
akija5 <arandjel3prodanovic@gmail.com>
2023-04-10 20:11:41 +0200

    ponovo main verzija

---
+++ SIMS-projekat-main/InitialProject/InitialProject/Migrations/20230323175106_GR.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InitialProject.Migrations
+{
+    public partial class GR : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AccomodationReservations_GuestRatings_GuestRatingId",
+                table: "AccomodationReservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AccomodationReservations_GuestRatingId",
+                table: "AccomodationReservations");
+
+            migrationBuilder.DropColumn(
+                name: "GuestRatingId",
+                table: "AccomodationReservations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccomodationReservationId",
+                table: "GuestRatings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GuestRatings_AccomodationReservationId",
+                table: "GuestRatings",
+                column: "AccomodationReservationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GuestRatings_AccomodationReservations_AccomodationReservationId",
+                table: "GuestRatings",
+                column: "AccomodationReservationId",
+                principalTable: "AccomodationReservations",
+                principalColumn: "AccomodationReservationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GuestRatings_AccomodationReservations_AccomodationReservationId",
+                table: "GuestRatings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GuestRatings_AccomodationReservationId",
+                table: "GuestRatings");
+
+            migrationBuilder.DropColumn(
+                name: "AccomodationReservationId",
+                table: "GuestRatings");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GuestRatingId",
+                table: "AccomodationReservations",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccomodationReservations_GuestRatingId",
+                table: "AccomodationReservations",
+                column: "GuestRatingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AccomodationReservations_GuestRatings_GuestRatingId",
+                table: "AccomodationReservations",
+                column: "GuestRatingId",
+                principalTable: "GuestRatings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ SIMS-projekat-main/InitialProject/InitialProject/Repository/TourRepository.cs
@@ -0,0 +1,184 @@
+﻿using InitialProject.DTO;
+using InitialProject.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.CodeDom;
+﻿using InitialProject.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Entities;
+
+namespace InitialProject.Repository
+{
+    public class TourRepository
+    {
+
+        TourReservationRepository tourReservationRepository = new TourReservationRepository();
+
+        public TourRepository() { }
+
+        public void AddTour(Tour tourToAdd)
+        {
+            using (var db = new DataContext())
+            {
+                db.Tours.Add(tourToAdd);
+                db.SaveChanges();
+            }
+        }
+
+        public List<Tour> GetAllTours()
+        {
+            using (var db = new DataContext())
+            {
+
+                return db.Tours.ToList();
+            }
+        }
+
+        public List<Tour> GetToursByLocation(int locationId)
+        {
+            List<Tour> toursByLocation = new List<Tour>();
+            using (var db = new DataContext())
+            {
+                var location = db.Locations.Include(t => t.Tours).SingleOrDefault(t => t.LocationId == locationId);
+                if (location != null)
+                {
+                    toursByLocation.AddRange(location.Tours);
+                }
+            }
+            return toursByLocation;
+        }
+
+        public int GetTouristsNumber(int tourId)
+        {
+            using (var db = new DataContext())
+            {
+                var tour = db.Tours.Include(t => t.TourReservations).SingleOrDefault(t => t.TourId == tourId);
+                
+                int tourists = 0;
+                List<TourReservation> tourReservations = tour.TourReservations.ToList();
+                foreach (TourReservation tourReservation in tourReservations)
+                {
+                    tourists += tourReservation.TouristsNumber;
+                }
+                return tourists;
+            }
+        }
+
+        public void BookTour(int tourId, int touristId, int tourists)
+        {
+            TourReservation tourReservation = new TourReservation(tourists);
+            using (var db = new DataContext())
+            {
+                Tourist tourist = db.Tourists.Find(touristId);
+                Tour tour = db.Tours.Find(tourId);
+                if (tourist != null && tour != null)
+                {
+                    tourist.TourReservations.Add(tourReservation);
+                    tour.TourReservations.Add(tourReservation);
+                    tour.Tourists.Add(tourist);
+                    db.SaveChanges();
+                }
+                
+            }
+            Console.WriteLine("Uspesno ste rezervisali turu.");
+        }
+
+        public Tour GetTourById(int id)
+        {
+            using (var db = new DataContext())
+            {
+                return db.Tours.FirstOrDefault(t => t.TourId == id);
+            }
+        }
+
+        public Tour GetTourByName(string name)
+        {
+            using (var db = new DataContext())
+            {
+                return db.Tours.FirstOrDefault(t => t.Name == name);
+            }
+        }
+
+        public void UpdateTour(int id, Tour updatedTour)
+        {
+            using (var context = new DataContext())
+            {
+                var tourToUpdate = context.Tours.FirstOrDefault(t => t.TourId == id);
+                if (tourToUpdate != null)
+                {
+                    tourToUpdate.Name = updatedTour.Name;
+                    tourToUpdate.Description = updatedTour.Description;
+                    tourToUpdate.Language = updatedTour.Language;
+                    tourToUpdate.MaxGuests = updatedTour.MaxGuests;
+                    tourToUpdate.StartTime = updatedTour.StartTime;
+                    tourToUpdate.EndTime = updatedTour.EndTime;
+                    tourToUpdate.Duration = updatedTour.Duration;
+                    tourToUpdate.Checkpoints = updatedTour.Checkpoints;
+                    tourToUpdate.Images = updatedTour.Images;
+                    tourToUpdate.Tourists = updatedTour.Tourists;
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void DeleteTour(int id)
+        {
+            using (var db = new DataContext())
+            {
+                var tourToDelete = db.Tours.FirstOrDefault(t => t.TourId == id);
+
+                if (tourToDelete != null)
+                {
+                    db.Tours.Remove(tourToDelete);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public List<Tour> GetToursByStartDate(DateTime startTime)
+        {
+            List<Tour> todaysTour = new List<Tour>();
+            using (var db = new DataContext())
+            {
+                todaysTour = db.Tours.Where(t => t.StartTime == startTime).Include(t => t.Checkpoints).Include(t=>t.Tourists).Include(t => t.Images).ToList();
+
+            }
+            return todaysTour;
+        }
+
+        public List<Tour> GetToursList()
+        {
+            List<Tour> toursWithCheckpoints = new List<Tour>();
+            using (var db = new DataContext())
+            {
+               toursWithCheckpoints = db.Tours.Include(t => t.Checkpoints).Include(t => t.Tourists).Include(t => t.Images).ToList();
+            }
+            return toursWithCheckpoints; 
+        }
+
+        public List<Tourist> GetTourists(Tour tour)
+        {
+            using (var db = new DataContext())
+            {
+                {
+                  
+                    var tourToReturn = db.Tours.Include(t => t.Tourists).FirstOrDefault(t => t.TourId == tour.TourId);
+
+                    
+                    if (tour == null)
+                    {
+                        return new List<Tourist>();
+                    }
+
+                    
+                    return tour.Tourists;
+                }
+            }
+        }
+    }
+}
