a2046eb64e064b9ea778cf5a1f453d8f22b22913
TamaraRanisovic <ranisovic123@gmail.com>
2023-04-19 00:34:07 +0200

    touristkt2

---
+++ InitialProject/InitialProject/Repository/DatesRepository.cs
@@ -0,0 +1,41 @@
+﻿using InitialProject.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Entities;
+
+namespace InitialProject.Repository
+{
+    public class DatesRepository
+    {
+        public DatesRepository() { }
+
+        public void Delete(Dates date)
+        {
+            using (var db = new DataContext())
+            {
+                db.Dates.Remove(date);
+                db.SaveChanges();
+            }
+        }
+
+        /*public List<Dates> GetAll()
+        {
+            using (var db = new DataContext())
+            {
+                return db.Dates.Include(t => t.Tours).ToList();
+            }
+        }*/
+
+        public Dates GetById(int id)
+        {
+            using (var db = new DataContext())
+            {
+                return db.Dates.FirstOrDefault(t => t.Id == id);
+            }
+        }
+    }
+}
