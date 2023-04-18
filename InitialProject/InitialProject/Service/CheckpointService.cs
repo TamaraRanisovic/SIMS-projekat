b7149e5c60bdb85496aad82f95968572a3edc9e6
TamaraRanisovic <ranisovic123@gmail.com>
2023-04-19 01:47:49 +0200

    kt2 - guidetourist merge

---
+++ InitialProject/InitialProject/Service/CheckpointService.cs
@@ -0,0 +1,34 @@
+﻿using InitialProject.Model;
+using InitialProject.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Entities;
+
+namespace InitialProject.Service
+{
+    public class CheckpointService
+    {
+        private readonly CheckpointRepository CheckpointRepository;
+        public CheckpointService() { }
+
+        public CheckpointService(CheckpointRepository checkpointRepository)
+        {
+            CheckpointRepository = checkpointRepository;
+        }
+        public List<Checkpoint> GetAllCheckpoints()
+        {
+            CheckpointRepository checkpointRepository = new CheckpointRepository();
+            return checkpointRepository.GetAllCheckpoints();
+        }
+
+        public List<Checkpoint> GetTourCheckpoints(int tourId)
+        {
+            CheckpointRepository checkpointRepository = new CheckpointRepository();
+            return checkpointRepository.GetTourCheckpoints(tourId);
+        }
+    }
+
+}
