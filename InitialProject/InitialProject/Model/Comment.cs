853b0a2b8cb895fbc49221c79ac6ff047983fb55
TamaraRanisovic <ranisovic123@gmail.com>
2023-04-19 03:08:18 +0200

    guidetouristkt2commit

---
+++ InitialProject/InitialProject/Model/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InitialProject.Model
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public DateTime CreationTime { get; set; }
+        public string Text { get; set; }
+
+        public Comment() { }
+
+        public Comment(DateTime creationTime, string text, User user)
+        {
+            CreationTime = creationTime;
+            Text = text;
+        }
+    
+
+    }
+}
